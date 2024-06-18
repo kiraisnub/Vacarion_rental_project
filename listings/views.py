076512a30ae002653d8c7b80ccde6becd2063403
kiraisnub <vrishinrajpurohit@gmail.com>
2024-06-18 13:52:11 +0530

    added loginmixin and usertestmixin and fixed the cards

--- listings/views.py
+++ listings/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView,CreateView,DetailView,ListView,DeleteView,UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
 from .models import HostLists
 from .forms import HostListForm
 from django.urls import reverse_lazy
@@ -10,10 +11,11 @@ from django.contrib.auth import get_user_model
 class HomeView(TemplateView):
     template_name = 'home.html'
 
-class CreateLisitngView(CreateView):
+class CreateLisitngView(LoginRequiredMixin,CreateView):
     model = HostLists
     template_name = 'CreateListing.html'
     form_class = HostListForm
+    login_url = 'login'
 
     def form_valid(self, form):
         form.instance.listing_host=self.request.user
@@ -21,20 +23,28 @@ class CreateLisitngView(CreateView):
 
     # fields = ('listing_name','listing_address','listing_price','listing_picture1','listing_picture2','listing_picture3','listing_host')
 
-class DetailLisitngView(DetailView):
+class DetailLisitngView(LoginRequiredMixin,DetailView):
     model = HostLists
     template_name = 'ListingDetail.html'
+    login_url = 'login'
 
 class ListingListView(ListView):
     model = HostLists
     template_name = 'ListingListView.html'
 
-class ListingUpdateView(UpdateView):
+class ListingUpdateView(UserPassesTestMixin,UpdateView):
     model = HostLists
     fields = ('listing_name', 'listing_price', 'listing_picture1','listing_picture2','listing_picture3')
     template_name = 'ListingUpdate.html'
+    def test_func(self):
+        obj=self.get_object()
+        return obj.listing_host==self.request.user
 
-class ListingDeleteView(DeleteView):
+class ListingDeleteView(UserPassesTestMixin,DeleteView):
     model = HostLists
     template_name = 'ListingDelete.html'
     success_url = reverse_lazy('ListingListView')
+
+    def test_func(self):
+        obj=self.get_object()
+        return obj.listing_host==self.request.user
