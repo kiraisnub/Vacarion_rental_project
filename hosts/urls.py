9e2ea720320937f0df2e5678dd49f41b1e3b2fce
kiraisnub <vrishinrajpurohit@gmail.com>
2024-06-24 23:02:29 +0530

    added user profile page, user listings and user update and delete pages

--- hosts/urls.py
+++ hosts/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from .views import Signupview,ProfileView,HostListings
+from .views import Signupview,ProfileView,HostListings,ProfileUpdateView,ProfileDeleteView
 from django.contrib.auth.views import LoginView
 from .forms import EmailOrUsernameAuthenticationForm
 
@@ -8,6 +8,8 @@ urlpatterns = [
     path('login/',LoginView.as_view(authentication_form=EmailOrUsernameAuthenticationForm),name='login'),
     path('signup/', Signupview.as_view(),name="signup"),
     path('profile/', ProfileView.as_view(),name="hosthome"),
+    path('profile/update', ProfileUpdateView.as_view(),name="profileupdate"),
+    path('profile/delete', ProfileDeleteView.as_view(),name="profiledelete"),
     path('listings/', HostListings.as_view(),name="my_listings"),
 
 ]
--- hosts/views.py
+++ hosts/views.py
@@ -1,5 +1,5 @@
 from .models import CustomHostUser
-from django.views.generic import CreateView,TemplateView,ListView
+from django.views.generic import CreateView,TemplateView,ListView,UpdateView,DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from listings.models import HostLists
 from .forms import CustomHostUserCreationForm,CustomHostUserChangeForm
@@ -11,15 +11,32 @@ class Signupview(CreateView):
     template_name = 'signup.html'
     success_url = reverse_lazy('home')
 
+class ProfileUpdateView(LoginRequiredMixin,UpdateView):
+    template_name = 'ProfileUpdate.html'
+    model = CustomHostUser
+    form_class = CustomHostUserChangeForm
+
+    def get_object(self, queryset=None):
+        return self.request.user
+    def get_success_url(self):
+        return reverse_lazy('hosthome')
+
+class ProfileDeleteView(LoginRequiredMixin,DeleteView):
+    template_name = "ProfileDelete.html"
+    model = CustomHostUser
+
+    def get_object(self, queryset=None):
+        return self.request.user
+    def get_success_url(self):
+        return reverse_lazy('home')
 class ProfileView(TemplateView):
     template_name = 'HostHome.html'
 
 class HostListings(LoginRequiredMixin,ListView):
     model = HostLists
-    template_name = "HostListings.html"
+    template_name = "HostListing.html"
     context_object_name = "listings"
 
     def get_queryset(self):
         host=self.request.user
-
-        return HostLists.object.filter(listing_host=host)
+        return HostLists.objects.filter(listing_host=host)
